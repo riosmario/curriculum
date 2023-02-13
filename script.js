0e00eefc694562d092beea338a1a3592253cec95
riosmario <rios.mario.o@hotmail.com>
2023-02-12 22:12:09 -0300

    no consigo que chrome me detecte el javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function alertaimagen() {
+  alert('Usted hizo click en REDES SOCIALES. Presione enter para continuar');
+}
+
+function alertamail() {
+  alert('Usted hizo click en E-MAIL. Presione enter para continuar');
+}
+
+document.getElementById('si').onclick = function() {
+  document.getElementById('gracias').innerText = "Gracias! tu opinion nos ayuda a mejorar!"
+}
+
+document.getElementById('no').onclick = function() {
+  document.getElementById('gracias').innerText = "Gracias por tu opinion! la proxima intentaremos hacerlo mejor!"
+}    
+  
+setInterval function() {
+  var elem = document.getElementById("blinking");
+  if (elem.style.visibility === "hidden") {
+    elem.style.visibility = "visible";
+  } else {
+    elem.style.visibility = "hidden";
+  }
+}
