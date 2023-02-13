d0117754e07f003c322f774ac910bf80c7269c17
riosmario <rios.mario.o@hotmail.com>
2023-02-12 22:22:50 -0300

    corregido. Faltaba parentesis en setInterval

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ document.getElementById('no').onclick = function() {
   document.getElementById('gracias').innerText = "Gracias por tu opinion! la proxima intentaremos hacerlo mejor!"
 }    
   
-setInterval function() {
+setInterval(function() {
   var elem = document.getElementById("blinking");
   if (elem.style.visibility === "hidden") {
     elem.style.visibility = "visible";
   } else {
     elem.style.visibility = "hidden";
   }
-}
+}, 500);
